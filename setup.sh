545ccfc6b2d3cd611b7228f72d06bdcef614226d
Zakiir Juman <zakiirjuman@gmail.com>
2023-02-22 14:20:59 -0500

    setup.sh test

--- setup.sh
+++ setup.sh
@@ -12,11 +12,13 @@ chmod o+rwx /archive_data/conf_list.yml
 #chmod +x src/init.js
 #chmod +x src/addFileToArchiveConf.js
 #chmod +x src/createNewArchiveConf.js
-ln -s src/init.js /usr/local/bin/node_backup
-ln -s src/addFileToArchiveConf.js /usr/local/bin/add_to_archive
-ln -s src/createNewArchiveConf.js /usr/local/bin/create_archive
-ln -s other/cproject.sh /usr/local/bin/create_project
-ln -s other/newuser.sh /usr/local/bin/adduser
+# get present working directory
+PWD=$(pwd)
+ln -s ${PWD}/src/init.js /usr/local/bin/node_backup
+ln -s ${PWD}/src/addFileToArchiveConf.js /usr/local/bin/add_to_archive
+ln -s ${PWD}/src/createNewArchiveConf.js /usr/local/bin/create_archive
+ln -s ${PWD}/other/cproject.sh /usr/local/bin/create_project
+ln -s ${PWD}/other/newuser.sh /usr/local/bin/adduser
 chmod o+x /usr/local/bin/node_backup
 chmod o+x /usr/local/bin/add_to_archive
 chmod o+x /usr/local/bin/create_archive
