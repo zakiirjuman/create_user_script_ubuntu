603cd1b12e9a944cdd5022e394ee627a5893911f
Zakiir Juman <zakiirjuman@gmail.com>
2023-02-22 14:02:36 -0500

    setup.sh to test

--- setup.sh
+++ setup.sh
@@ -12,13 +12,13 @@ chmod o+rwx /archive_data/conf_list.yml
 #chmod +x src/init.js
 #chmod +x src/addFileToArchiveConf.js
 #chmod +x src/createNewArchiveConf.js
-#ln -s src/init.js /usr/local/bin/node_backup
-#ln -s src/addFileToArchiveConf.js /usr/local/bin/add_to_archive
-#ln -s src/createNewArchiveConf.js /usr/local/bin/create_archive
-#ln -s other/cproject.sh /usr/local/bin/create_project
-#ln -s other/newuser.sh /usr/local/bin/adduser
-#chmod o+x /usr/local/bin/node_backup
-#chmod o+x /usr/local/bin/add_to_archive
-#chmod o+x /usr/local/bin/create_archive
-#chmod o+x /usr/local/bin/create_project
-#chmod o+x /usr/local/bin/adduser
+ln -s src/init.js /usr/local/bin/node_backup
+ln -s src/addFileToArchiveConf.js /usr/local/bin/add_to_archive
+ln -s src/createNewArchiveConf.js /usr/local/bin/create_archive
+ln -s other/cproject.sh /usr/local/bin/create_project
+ln -s other/newuser.sh /usr/local/bin/adduser
+chmod o+x /usr/local/bin/node_backup
+chmod o+x /usr/local/bin/add_to_archive
+chmod o+x /usr/local/bin/create_archive
+chmod o+x /usr/local/bin/create_project
+chmod o+x /usr/local/bin/adduser
